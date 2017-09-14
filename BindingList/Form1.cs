22be0c64373c49b8e53d04698cf4223b470a4924
Arrival Sentosa <arrivaldwisentosa@gmail.com>
2017-09-14 10:25:18 +0700

    Initial commit

---
+++ BindingList/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BindingList
+{
+    public partial class Form1 : Form
+    {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+        BindingList<Product> blProduct = new BindingList<Product>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            blProduct = new BindingList<Product>(db.Products.ToList());
+            dataGridView1.DataSource = blProduct;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            blProduct[0].Name = "test";
+            blProduct.RemoveAt(0);
+            db.SubmitChanges();
+        }
+    }
+}
